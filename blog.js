58ec04c7d1067f562a4823e3ab14fb0599068fb6
PieStw <piero.stalteri@icloud.com>
2024-11-19 14:29:09 +0100

    aggiunto metodo di risposta standard

--- blog.js
+++ blog.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen();
+
+app.get("/", (req, res) => {
+    res.send("Server del mio Blog");
+});
+
+
+app.listen(port, () => {
+    console.log("Server online sulla porta: " + port);
+});
