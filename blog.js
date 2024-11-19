bac39631d2404d555dce0df6439cf969a3895bc8
PieStw <piero.stalteri@icloud.com>
2024-11-19 14:44:43 +0100

    creazione metodo /bacheca

--- blog.js
+++ blog.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
     res.send("Server del mio Blog");
 });
 
+app.get("/bacheca", (req, res) => {
+
+    const lista = [
+        {titolo: "che bella gionata", immagine: "img/ciambellone.jpeg", contenuto: "", tags: []},
+        {titolo: "che caldo", immagine: "img/cracker_barbabietola.jpeg", contenuto: "", tags: []},
+        {titolo: "che freddo", immagine: "img/pane_fritto_dolce.jpeg", contenuto: "", tags: []},
+        {titolo: "ciao", immagine: "img/pasta_barbabietola.jpeg", contenuto: "", tags: []},
+        {titolo: "ho fame", immagine: "img/torta_paesana.jpeg", contenuto: "", tags: []},
+    ];
+
+
+
+    res.json({posts: lista, numeroElementi: lista.length});
+});
+
 
 app.listen(port, () => {
     console.log("Server online sulla porta: " + port);
