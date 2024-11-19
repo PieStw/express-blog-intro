881f5562ce553f27967f040dd981783f5dbedd0f
PieStw <piero.stalteri@icloud.com>
2024-11-19 14:56:32 +0100

    aggiunta possibilita di filtrare con un parametro

--- blog.js
+++ blog.js
@@ -9,17 +9,21 @@ app.get("/", (req, res) => {
 
 app.get("/bacheca", (req, res) => {
 
-    const lista = [
-        {titolo: "che bella gionata", immagine: "img/ciambellone.jpeg", contenuto: "", tags: []},
-        {titolo: "che caldo", immagine: "img/cracker_barbabietola.jpeg", contenuto: "", tags: []},
-        {titolo: "che freddo", immagine: "img/pane_fritto_dolce.jpeg", contenuto: "", tags: []},
-        {titolo: "ciao", immagine: "img/pasta_barbabietola.jpeg", contenuto: "", tags: []},
-        {titolo: "ho fame", immagine: "img/torta_paesana.jpeg", contenuto: "", tags: []},
+    const posts = [
+        {titolo: "che bella gionata", immagine: "img/ciambellone.jpeg", contenuto: "non so che scriverci", tags: ["sole", "mare"]},
+        {titolo: "che caldo", immagine: "img/cracker_barbabietola.jpeg", contenuto: "non so che scriverci", tags: ["mare"]},
+        {titolo: "che freddo", immagine: "img/pane_fritto_dolce.jpeg", contenuto: "non so che scriverci", tags: ["radio"]},
+        {titolo: "ciao", immagine: "img/pasta_barbabietola.jpeg", contenuto: "non so che scriverci", tags: ["vip"]},
+        {titolo: "ho fame", immagine: "img/torta_paesana.jpeg", contenuto: "non so che scriverci", tags: ["tv"]},
     ];
 
+    const tag = req.query.tag;
 
+    const newPosts = posts.filter((post) => {
+        return post.tags.includes(tag.toLowerCase())
+    })
 
-    res.json({posts: lista, numeroElementi: lista.length});
+    res.json({posts: newPosts, numeroElementi: newPosts.length});
 });
 
 
